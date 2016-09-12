32581a8d1d19932ad50a2fec1e3ed0f2bbb23e90
aliang1122 <aliang1122@hotmail.com>
2016-09-12 19:44:04 +0800

    modified api.js

--- lesson6_exercise/lib/routers/api.js
+++ lesson6_exercise/lib/routers/api.js
@@ -8,25 +8,27 @@ router.route('/')
 
 router.route('/:id')
     .get(function(req,res,next){
-      var id=req.params.id;
-      if(id<1||id>newsModel.length){
-        res.status(404).send('Not Exist')
+      var id=req.params.id-1;
+      var news=newsModel[id];
+      if(news){
+        res.status(200).send(news);
       }else{
-        res.status(200).send(newsModel[id-1]);
+        res.status(404).send('Not Exist');
       }
 
     });
 
 router.route('/:id')
     .delete(function(req,res,next){
-      var id=req.params.id;
-      if(newsModel[id-1]){
-        newsModel.splice(id-1,1);
+      var id=req.params.id-1;
+      var news=newsModel[id];
+      if(news){
+        newsModel.splice(id,1);
         console.log('delete successfully');
         console.log('删除后的数据为：',newsModel);
         res.status(200).end();
       }else{
-        res.status(403).send('error, data not found');
+        res.status(404).send('error, data not found');
       }
     });
 
