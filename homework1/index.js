be37812ddb2c8c4f5b62250eaf9226c897948d4b
aliang1122 <aliang1122@hotmail.com>
2016-09-15 14:03:59 +0800

    beautiful code

--- homework1/index.js
+++ homework1/index.js
@@ -1,13 +1,13 @@
-var app=require('express')(),
-    bodyParser=require('body-parser');
+var app = require('express')(),
+    bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
-  extended:false
+    extended: false
 }));
 
-app.use('/user',require('./lib/routers/user'));
-app.use('/album',require('./lib/routers/album'));
+app.use('/user', require('./lib/routers/user'));
+app.use('/album', require('./lib/routers/album'));
 
-app.listen(3000,function(){
-  console.log("app is running...");
+app.listen(3000, function() {
+    console.log("app is running...");
 });
